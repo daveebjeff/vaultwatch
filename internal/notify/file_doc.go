a6f604b6cfe0a3e751a0b9600bc33b448e369356
Dave Jefferson <daveebjeff@users.noreply.github.com>
2026-04-19 21:22:59 +0800

    docs: add Notifier interface contract to package doc comment

--- internal/notify/file_doc.go
+++ internal/notify/file_doc.go
@@ -12,4 +12,15 @@
 //
 // Both notifiers implement the Notifier interface and can be composed with
 // MultiNotifier to fan out to multiple destinations simultaneously.
+//
+// # Notifier Interface
+//
+// All notifiers in this package satisfy the following interface:
+//
+//	type Notifier interface {
+//		Notify(ctx context.Context, msg Message) error
+//		Close() error
+//	}
+//
+// Implementations should be safe for concurrent use unless otherwise noted.
 package notify
